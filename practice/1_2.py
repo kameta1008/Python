92e0241edf1afb1f04f5769e4818f77ecce079e9
kameta1008 <takemarue202@gmail.com>
2024-12-16 23:37:58 +0900

    辞書の演習

--- practice/1_2.py
+++ practice/1_2.py
@@ -1,2 +1,5 @@
-scores = {"数学":82, "国語":74, "英語":60, "理科":92, 70}
-avg_score = sum(scores) / len(scores)
+scores = {"数学":82, "国語":74, "英語":60, "理科":92, "社会":70}
+
+scores_values = list(scores.values())
+avg_score = sum(scores_values) / len(scores_values)
+print(f"{avg_score} 点")
